27caaa4545196b5f4608849831fe651a7b5beb9c
Sydney Kirtley <59988591+sydneymkirtley@users.noreply.github.com>
2020-05-19 21:10:52 -0400

    Update LoggerCDI.ino

--- LoggerCDI.ino
+++ LoggerCDI.ino
@@ -1,26 +1,27 @@
-#include <BluetoothSerial.h>
+#include <BluetoothSerial.h> //this library allows use of the Bluetooth serial features of the esp32
 
-BluetoothSerial SerialBT;
+BluetoothSerial SerialBT; //rename the Bluetooth serial features to be more recognizable
 
 void setup() {
-  Serial.begin(115200);
-  Serial1.begin(115200);
-  SerialBT.register_callback(callback);
+  Serial.begin(115200); //start serial communication with the Arduino serial monitor
+  Serial1.begin(115200); //start serial communication with the CDI port (defaults to the TX/RX pins)
+  SerialBT.register_callback(callback); //call the callback function -- has to to do with the connection over Bluetooth
 
   if(!SerialBT.begin("ESP32")){
-    Serial.println("An error occurred initializing Bluetooth");
+    Serial.println("An error occurred initializing Bluetooth"); //if the serial communication with Bluetooth doesn't begin, print this to Arduino monitor
   }else{
-    Serial.println("Bluetooth initialized");
+    Serial.println("Bluetooth initialized"); //if the serial communication with Bluetooth does begin, print this to the Arduino monitor
   }
 }
 
-  while(1){
-    if (Serial1.available()) {
-        SerialBT.write(Serial1.read());
+  void loop (){
+    while(1){
+      if (Serial1.available()) {
+          SerialBT.write(Serial1.read()); //if there is data available over CDI, write it to the Bluetooth monitor
+      }
     }
-}
-
-void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  }
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){ //function to check if something is connected to the Bluetooth
   if(event == ESP_SPP_SRV_OPEN_EVT){
     SerialBT.println("Client Connected");
   }
@@ -29,4 +30,3 @@ void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
     SerialBT.println("Client disconnected");
   }
 }
-//end
